fee07237f8fa2a27b91ee2ba6c5eb7d9e3aa275b
JanDongGun <donggontrannguyen@gmail.com>
2021-12-07 14:16:51 +0700

    first commit

---
+++ lib/pages/root_app.dart
@@ -0,0 +1,80 @@
+import 'package:budgetapp/pages/daily_page.dart';
+import 'package:budgetapp/theme/colors.dart';
+import 'package:flutter/material.dart';
+import 'package:ionicons/ionicons.dart';
+import 'package:animated_bottom_navigation_bar/animated_bottom_navigation_bar.dart';
+
+class RootApp extends StatefulWidget {
+  const RootApp({Key? key}) : super(key: key);
+
+  @override
+  _RootAppState createState() => _RootAppState();
+}
+
+class _RootAppState extends State<RootApp> {
+  int pageIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: getBody(),
+      bottomNavigationBar: getFooter(),
+      floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            setTabs(4);
+          },
+          child: const Icon(Icons.add, size: 25),
+          backgroundColor: primary),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+    );
+  }
+
+  Widget getBody() {
+    return IndexedStack(
+      index: pageIndex,
+      children: const [
+        DailyPage(),
+        Center(
+          child: Text("Stats Page"),
+        ),
+        Center(
+          child: Text("Budget Page"),
+        ),
+        Center(
+          child: Text("Profile page"),
+        ),
+        Center(
+          child: Text("Create budget Page"),
+        ),
+      ],
+    );
+  }
+
+  Widget getFooter() {
+    List<IconData> iconItems = [
+      Ionicons.calendar,
+      Ionicons.stats_chart,
+      Ionicons.wallet,
+      Ionicons.person
+    ];
+    return AnimatedBottomNavigationBar(
+        icons: iconItems,
+        activeColor: primary,
+        inactiveColor: black.withOpacity(0.5),
+        activeIndex: pageIndex,
+        splashColor: secondary,
+        notchSmoothness: NotchSmoothness.softEdge,
+        gapLocation: GapLocation.center,
+        leftCornerRadius: 10,
+        iconSize: 25,
+        rightCornerRadius: 10,
+        onTap: (index) {
+          setTabs(index);
+        });
+  }
+
+  setTabs(index) {
+    setState(() {
+      pageIndex = index;
+    });
+  }
+}
