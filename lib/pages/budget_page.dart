b04c0cbca78702ef281b8e9b7325b5f1f32f201a
Phạm Trung Anh Dũng <dungpham4320@gmail.com>
2021-12-07 18:44:34 +0700

    add budgetContainer Widget

--- lib/pages/budget_page.dart
+++ lib/pages/budget_page.dart
@@ -1,9 +1,9 @@
-import 'package:budgetapp/json/daily_json.dart';
 import 'package:budgetapp/json/day_month.dart';
 import 'package:budgetapp/theme/colors.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:antdesign_icons/antdesign_icons.dart';
+import 'package:percent_indicator/linear_percent_indicator.dart';
 
 class Budget extends StatefulWidget {
   const Budget({Key? key}) : super(key: key);
@@ -14,6 +14,8 @@ class Budget extends StatefulWidget {
 
 class _BudgetState extends State<Budget> {
   int activeDay = 4;
+  // int amount = 6000;
+  // int currentAmount = 3000;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,7 +26,6 @@ class _BudgetState extends State<Budget> {
 
   Widget getBody() {
     var size = MediaQuery.of(context).size;
-
     return SingleChildScrollView(
         child: Column(
       children: [
@@ -108,7 +109,74 @@ class _BudgetState extends State<Budget> {
             ),
           ),
         ),
+        const SizedBox(
+          height: 20,
+        ),
+        budgetContainer("Budget 1", 2500, 3000),
+        budgetContainer("Budget 2", 1200, 2500),
+        budgetContainer("Budget 3", 700, 2300),
       ],
     ));
+  }
+
+  Widget budgetContainer(String budgetName, int currentAmount, int amount) {
+    var size = MediaQuery.of(context).size;
+    var percentAmount = (currentAmount / amount) * 100;
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              budgetName,
+              style: TextStyle(color: Colors.grey),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Text(
+                  "\$" + currentAmount.toString(),
+                  style: TextStyle(fontSize: 30),
+                ),
+                const SizedBox(
+                  width: 5,
+                ),
+                Text(
+                  percentAmount.toStringAsFixed(0) + "%",
+                  style: TextStyle(color: Colors.grey),
+                ),
+                Spacer(),
+                Text(
+                  "\$" + amount.toString(),
+                  style: TextStyle(color: Colors.grey),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            LinearPercentIndicator(
+              percent: percentAmount / 100,
+              progressColor: blue,
+            )
+          ],
+        ),
+        width: (size.width - 40),
+        height: (size.height) / 7,
+        padding: EdgeInsets.all(15.0),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+                color: grey.withOpacity(0.05), spreadRadius: 3, blurRadius: 3),
+          ],
+        ),
+      ),
+    );
   }
 }
