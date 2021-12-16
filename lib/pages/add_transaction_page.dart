68a82520bea24991e61f4ec1d2037c93f7a499b1
Phạm Trung Anh Dũng <dungpham4320@gmail.com>
2021-12-16 21:30:17 +0700

    update image

--- lib/pages/add_transaction_page.dart
+++ lib/pages/add_transaction_page.dart
@@ -53,113 +53,130 @@ class _AddTransactionState extends State<AddTransaction> {
               ),
             ),
           ),
-          Container(
-              child: Image.asset('assets/images/credit_card_illustration.png')),
-          Text('What kind of transaction it is ?',
-              style: TextStyle(
-                  fontSize: 20,
-                  color: grey.withOpacity(1),
-                  fontWeight: FontWeight.bold)),
           const SizedBox(
             height: 20,
           ),
           Container(
-            width: size.width - 40,
-            height: size.height / 4.5,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              child: Image.asset('assets/images/credit_card_illustration.png')),
+          const SizedBox(
+            height: 20,
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(30, 10, 30, 10),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                GestureDetector(
-                  onTap: () {},
-                  child: Container(
-                    width: (size.width - 60) / 2,
-                    height: size.height / 4.5,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: Colors.white,
-                      boxShadow: [
-                        BoxShadow(
-                            color: grey.withOpacity(0.05),
-                            spreadRadius: 3,
-                            blurRadius: 3),
-                      ],
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.all(20.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Container(
-                            height: 40,
-                            width: 40,
-                            child: FloatingActionButton(
-                              onPressed: () {},
-                              child: Icon(Icons.first_page),
-                              backgroundColor: blue,
-                              foregroundColor: white,
+                Container(
+                  width: size.width / 2,
+                  child: Text('What kind of transaction it is ?',
+                      style: TextStyle(
+                          fontSize: 20,
+                          color: black.withOpacity(0.6),
+                          fontWeight: FontWeight.bold)),
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                Container(
+                  width: size.width - 40,
+                  height: size.height / 4.5,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      GestureDetector(
+                        onTap: () {},
+                        child: Container(
+                          width: (size.width - 100) / 2,
+                          height: size.height / 4.5,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: Colors.white,
+                            boxShadow: [
+                              BoxShadow(
+                                  color: grey.withOpacity(0.05),
+                                  spreadRadius: 3,
+                                  blurRadius: 3),
+                            ],
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.all(20.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Container(
+                                  height: 40,
+                                  width: 40,
+                                  child: FloatingActionButton(
+                                    onPressed: () {},
+                                    child: Icon(Icons.first_page),
+                                    backgroundColor: blue,
+                                    foregroundColor: white,
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: size.height / 20,
+                                ),
+                                Text('Income',
+                                    style: TextStyle(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.w500,
+                                        color: Colors.black87.withOpacity(1)))
+                              ],
                             ),
                           ),
-                          SizedBox(
-                            height: size.height / 20,
-                          ),
-                          Text('Income',
-                              style: TextStyle(
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.w500,
-                                  color: Colors.black87.withOpacity(1)))
-                        ],
+                        ),
                       ),
-                    ),
+                      GestureDetector(
+                        onTap: () {},
+                        child: Container(
+                          width: (size.width - 100) / 2,
+                          height: size.height / 4.5,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: Colors.white,
+                            boxShadow: [
+                              BoxShadow(
+                                  color: grey.withOpacity(0.05),
+                                  spreadRadius: 3,
+                                  blurRadius: 3),
+                            ],
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.all(20.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Container(
+                                  height: 40,
+                                  width: 40,
+                                  child: FloatingActionButton(
+                                    onPressed: () {},
+                                    child: Icon(Icons.last_page),
+                                    backgroundColor: red,
+                                    foregroundColor: white,
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: size.height / 20,
+                                ),
+                                Text('Expense',
+                                    style: TextStyle(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.w500,
+                                        color: Colors.black87.withOpacity(1)))
+                              ],
+                            ),
+                          ),
+                        ),
+                      )
+                    ],
                   ),
                 ),
-                GestureDetector(
-                  onTap: () {},
-                  child: Container(
-                    width: (size.width - 60) / 2,
-                    height: size.height / 4.5,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: Colors.white,
-                      boxShadow: [
-                        BoxShadow(
-                            color: grey.withOpacity(0.05),
-                            spreadRadius: 3,
-                            blurRadius: 3),
-                      ],
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.all(20.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Container(
-                            height: 40,
-                            width: 40,
-                            child: FloatingActionButton(
-                              onPressed: () {},
-                              child: Icon(Icons.last_page),
-                              backgroundColor: red,
-                              foregroundColor: white,
-                            ),
-                          ),
-                          SizedBox(
-                            height: size.height / 20,
-                          ),
-                          Text('Expense',
-                              style: TextStyle(
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.w500,
-                                  color: Colors.black87.withOpacity(1)))
-                        ],
-                      ),
-                    ),
-                  ),
-                )
               ],
             ),
-          )
+          ),
         ],
       ),
     );
